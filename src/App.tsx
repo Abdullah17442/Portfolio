1ed016e9fdd31da3afac6497759cf9a027f95cca
Abdullah Ahmed <132512081+Abdullah17442@users.noreply.github.com>
2026-09-08 00:04:38 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -180,7 +180,7 @@ function App() {
 
             <a
               href={resume}
-              download="Abdullah-Ahmed-Resume.pdf"
+              download="Resume - Abdullah-Ahmed.pdf"
             >
               CV ↓
             </a>
@@ -235,7 +235,7 @@ function App() {
               <a
                 className="btn"
                 href={resume}
-                download="Abdullah-Ahmed-Resume.pdf"
+                download="Resume - Abdullah-Ahmed.pdf"
               >
                 CV
               </a>
@@ -739,7 +739,7 @@ function App() {
 
             <a
               href={resume}
-              download="Abdullah-Ahmed-Resume.pdf"
+              download="Resume - Abdullah-Ahmed.pdf"
             >
               CV
             </a>
